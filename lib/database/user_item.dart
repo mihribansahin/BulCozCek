0abcb2da6ba95e52b39721be435db189b6e8983d
mihribansahin <mishsapp@gmail.com>
2021-11-24 21:55:45 +0300

    created db models

--- lib/database/user_item.dart
+++ lib/database/user_item.dart
@@ -2,30 +2,35 @@ import 'dart:collection';
 
 import 'package:bul_coz_cek_game/database/tables.dart';
 
-class UserItem{
+class UserItem {
+  int? id;
+  String? username;
+  int? point;
 
+  Map<dynamic, dynamic> userItemToMap() {
+    var userItems = Map<String, dynamic>();
 
-   int? id;
-   String? username;
-   int? point;
+    userItems[Users.id] = id;
+    userItems[Users.username] = username;
+    userItems[Users.point] = point;
+    return userItems;
+  }
 
+  UserItem.fromMap(HashMap<String, dynamic> map) {
+    this.id = map[Users.id];
+    this.username = map[Users.username];
+    this.point = map[Users.point];
+  }
 
+  UserItem.fromJsonMap(Map<String, dynamic> json) {
+    this.id = json[Users.id];
+    this.username = json[Users.username];
+    this.point = json[Users.point];
+  }
 
-   Map<dynamic, dynamic> userItemToMap() {
-     var userItems = Map<String, dynamic>();
-
-     userItems[Users.id] = id;
-     userItems[Users.username] = username;
-     userItems[Users.point] = point;
-     return userItems;
-   }
-
-   UserItem.fromMap(HashMap<String, dynamic> map) {
-     this.id = map[Users.id];
-     this.username = map[Users.username];
-     this.point = map[Users.point];
-   }
-
-
-
+  Map<String, dynamic> toJson() => {
+        Users.id: this.id,
+        Users.username: this.username,
+        Users.point: this.point,
+      };
 }
